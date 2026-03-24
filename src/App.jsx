6b54cff6216f61d0c2e19cf01c33c4e69d958d36
Kais Tarraf <hossamtr184@gmail.com>
2026-03-24 13:40:49 +0300

    Implement Experience section

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ const App = () => {
 				<Hero />
 				<About />
 				<Projects />
-				{/*<Experience />
-				<Testimonials />
+				<Experience />
+				{/*<Testimonials />
 				<Contact /> */}
 			</main>
 		</div>
